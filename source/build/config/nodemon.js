d640352f1237418f5a55d89c1b8098d49aea8330
unknown <tchen@DSGDYN7Q52.prod.quest.corp>
2015-12-22 17:35:20 +0800

    增加nodemon watch

--- source/build/config/nodemon.js
+++ source/build/config/nodemon.js
@@ -7,13 +7,19 @@ gulp.task('nodemon', function () {
     nodemon({
         script: setting.dest.root + 'bin/www',
         ext: 'html css js',
+        watch: [
+            setting.dest.root + 'bin',
+            setting.dest.root + 'models',
+            setting.dest.root + 'routes',
+            setting.dest.root + 'app.js',
+            setting.dest.root + 'db.js'
+        ],
         ignore: [
             setting.dest.root + 'node_modules/**'
         ]
     })
-        .on('start', function (cb) {
+        .on('start', function () {
             console.log('start');
-            cb();
         })
         .on('restart', function () {
             console.log('restarted');
