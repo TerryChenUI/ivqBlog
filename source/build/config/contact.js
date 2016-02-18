2c2255533c015df9d011c426603481150e385ae4
unknown <tchen@DSGDYN7Q52.prod.quest.corp>
2016-02-18 16:11:08 +0800

    更新contact配置

--- source/build/config/contact.js
+++ source/build/config/contact.js
@@ -7,9 +7,7 @@ var gulp = require('gulp'),
     setting = require('../setting'),
     libraries = require('./common');
 
-gulp.task('concat', ['contact:front', 'contact:admin']);
-gulp.task('contact:front', ['front:js', 'front:third:css', 'front:third:js']);
-gulp.task('contact:admin', ['admin:js', 'admin:third:css', 'admin:third:js']);
+gulp.task('concat', ['front:js', 'admin:js']);
 
 //front
 gulp.task('front:js', function () {
@@ -20,42 +18,10 @@ gulp.task('front:js', function () {
         .pipe(gulp.dest(setting.dest.root));
 });
 
-gulp.task('front:third:css', function () {
-    return gulp.src(libraries.front.css)
-        .pipe(concat('third-party.css'))
-        .pipe(minifyCss())
-        .pipe(rev())
-        .pipe(gulp.dest(setting.dest.root));
-});
-
-gulp.task('front:third:js', function () {
-    return gulp.src(libraries.front.js)
-        .pipe(concat('third-party.js'))
-        .pipe(uglify())
-        .pipe(rev())
-        .pipe(gulp.dest(setting.dest.root));
-});
-
 //admin
 gulp.task('admin:js', function () {
     return gulp.src(['webui/admin/app/app.js', 'webui/admin/app/**/*.js', 'webui/common/**/*.js', 'webui/admin/common/**/*.js', '!webui/admin/app/**/*.spec.js'])
         .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(rev())
-        .pipe(gulp.dest(setting.dest.admin));
-});
-
-gulp.task('admin:third:css', function () {
-    return gulp.src(libraries.front.css)
-        .pipe(concat('third-party.css'))
-        .pipe(minifyCss())
-        .pipe(rev())
-        .pipe(gulp.dest(setting.dest.admin));
-});
-
-gulp.task('admin:third:js', function () {
-    return gulp.src(libraries.admin.js)
-        .pipe(concat('third-party.js'))
         .pipe(uglify())
         .pipe(rev())
         .pipe(gulp.dest(setting.dest.admin));
